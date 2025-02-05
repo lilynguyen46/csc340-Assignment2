35b11cecc1a8372f9f5dc4b6023994e8657227c8
Lily Nguyen <lhnguyen2@uncg.edu>
2025-02-05 16:22:53 -0500

    Third commit

--- src/main/java/csc340/demo/Controller.java
+++ src/main/java/csc340/demo/Controller.java
@@ -1,4 +1,22 @@
 package csc340.demo;
 
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+
+@RestController
+@RequestMapping("/api")
 public class Controller {
+
+    private static final String DB_COCKTAILS = "www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita\n";
+
+
+    @GetMapping("/Margarita")
+    public String getMagaritaInfo() {
+        RestTemplate restTemplate = new RestTemplate();
+        return restTemplate.getForObject(DB_COCKTAILS, String.class);
+    }
 }
